4f0a57cb38013ddeab006f3f22fc7cad0d08c989
Vladyslav Novak <43440712+twistik@users.noreply.github.com>
2024-06-18 15:12:29 +0300

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
 
 <template>
   <main>
-    <header class="font-bold text-xl rounded overflow-hidden shadow-lg px-12 py-6 flex justify-between items-center">Test task Lazy Ants</header>
+    <header class="font-bold text-xl rounded overflow-hidden shadow-lg px-12 py-6 flex justify-between items-center">Simple App</header>
     <slot></slot>
   </main>
 </template>
